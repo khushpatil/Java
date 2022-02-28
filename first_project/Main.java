d322265f57fa3e1f2a0ff63cc713cb7dd90aacb9
khushpatil <khushpatil70@gmail.com>
2022-02-28 13:03:23 +0530

    Made some simple programs in Java

---
+++ first_project/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Nice meeting you " + name);
+        int count = 1;
+        while(count < 5){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
---
+++ first_project/Temp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Temp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter temperature in celcius: ");
+        float tempc = input.nextFloat();
+        double tempf = tempc * 1.8 + 32;
+        System.out.print("The temperature in Farenheit is: " + tempf);
+    }
+
+}
